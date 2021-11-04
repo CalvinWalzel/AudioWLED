1cfde4f8de13b3368b446d5ce716f209bf1cace1
Calvin <wow.busk@gmail.com>
2021-11-04 20:15:23 +0100

    Add project files.

---
+++ AudioWLED/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AudioWLED
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly AudioObserver audioObserver;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            audioObserver = new AudioObserver();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadAudioInterfaceItems();
+        }
+
+        private void LoadAudioInterfaceItems()
+        {
+            MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+
+            for (int i = 0; i < WaveOut.DeviceCount; i++)
+            {
+                MMDevice device = enumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active)[i];
+                cBoxAudioInterfaces.Items.Add(device.ToString());
+            }
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            audioObserver.Start();
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            audioObserver.Stop();
+        }
+    }
+}
---
+++ AudioWLEDConsole/Program.cs
@@ -0,0 +1,80 @@
+﻿using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using System;
+using System.Net.Http;
+using System.Threading;
+
+namespace AudioWLED
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        // add settings persistence
+        // add state changed
+        // minimize to system tray
+        // https://www.codeproject.com/Questions/354327/Minimizing-Console-Window-to-System-Tray
+
+        static void Main(string[] args)
+        {
+            var defaultIp = "10.0.0.74";
+            Console.WriteLine("Enter an IP (default: {0}):", defaultIp);
+            var ip = Console.ReadLine();
+
+            if (String.IsNullOrWhiteSpace(ip)) ip = defaultIp;
+
+            var enumerator = new MMDeviceEnumerator();
+            // cycle trough all audio devices
+            for (int i = 0; i < WaveOut.DeviceCount; i++)
+                Console.WriteLine("[{0}] {1}", i, enumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active)[i]);
+            var index = Convert.ToInt32(Console.ReadLine());
+
+            var device = enumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active)[index];
+
+            enumerator.Dispose();
+
+            var capture = new WasapiLoopbackCapture(device);
+
+            capture.DataAvailable += (object sender, WaveInEventArgs a) =>
+            {
+                var _buffer = a.Buffer;
+
+                double sum = 0;
+                for (var i = 0; i < _buffer.Length; i = i + 2)
+                {
+                    double sample = BitConverter.ToInt16(_buffer, i) / 3276.0;
+                    sum += (sample * sample);
+                }
+                double rms = Math.Sqrt(sum / (_buffer.Length / 2));
+                var decibel = 20 * Math.Log10(rms);
+
+                Console.WriteLine(decibel);
+
+                if (Double.IsNegativeInfinity(decibel))
+                {
+                    Console.WriteLine("Silence");
+
+                    client.GetStringAsync("http://" + ip + "/win&A=0");
+                } else
+                {
+                    Console.WriteLine("Party!");
+
+                    client.GetStringAsync("http://" + ip + "/win&A=128");
+                }
+            };
+
+            capture.RecordingStopped += (object sender, StoppedEventArgs e) =>
+            {
+                capture.StartRecording();
+            };
+
+            capture.StartRecording();
+
+            while (true)
+            {
+                Thread.Sleep(500);
+            }
+        }
+
+    }
+}
